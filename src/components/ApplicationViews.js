306b388d0446c2fa2d9a4d438e7da9ea490e17fd
Daniel Krusch <daniel.krusch@gmail.com>
2019-07-29 10:36:21 -0500

    Added a welcome page that has to buttons that route to login and register pages

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,186 @@
+import { Route, Redirect } from "react-router-dom"
+import React, { Component } from "react"
+import Welcome from "./welcome/Welcome"
+import Login from "./welcome/Login"
+import Register from "./welcome/Register"
+import APIManager from "../modules/APIManager"
+import { withRouter } from "react-router"
+
+class ApplicationViews extends Component {
+  state = {
+    locations: [],
+    friends: []
+  }
+
+
+  //Methods to be passed to components
+  likeItem = (name, word) => {
+    console.log("inside delete item")
+    let newObj = {}
+    return APIManager.getLike(name, word)
+    .then(group => {
+      console.log(group)
+      newObj["potentialFriends"] = group
+      this.setState(newObj)
+      console.log(name, newObj, this.state)
+      this.props.history.push(`/friends/search`)
+    })
+  }
+
+
+  deleteItem = (name, id) => {
+    console.log("inside delete item")
+    let newObj = {}
+    return fetch(`http://localhost:5002/${name}/${id}`, {
+      method: "DELETE"
+    })
+      .then(e => e.json())
+      .then(() => APIManager.getAll(`${name}?user_id=${+sessionStorage.getItem("activeUser")}`
+      ))
+      .then(group => {
+        newObj[name] = group
+        this.setState(newObj)
+        console.log(name, newObj, this.state)
+        this.props.history.push(`/${name}`)
+      })
+  }
+  deleteMessage = (name, id) => {
+    console.log("inside delete item")
+    let newObj = {}
+    return fetch(`http://localhost:5002/${name}/${id}`, {
+      method: "DELETE"
+    })
+      .then(e => e.json())
+      .then(() => APIManager.getAll(`${name}`
+      ))
+      .then(group => {
+        newObj[name] = group
+        this.setState(newObj)
+        console.log(name, newObj, this.state)
+        this.props.history.push(`/${name}`)
+      })
+  }
+  updateItem = (name, editedObject) => {
+    let newObj = {}
+    return APIManager.put(name, editedObject)
+      .then(() =>
+        APIManager.getAll(
+          `${name}?user_id=${+sessionStorage.getItem("activeUser")}`
+        )
+      )
+      .then(item => {
+        newObj[name] = item
+        this.setState(newObj)
+      })
+      .then(() => this.props.history.push(`/${name}`))
+  }
+  updateMessage = (name, editedObject) => {
+    let newObj = {}
+    return APIManager.put(name, editedObject)
+      .then(() =>
+        APIManager.getAll(
+          `${name}`
+        )
+      )
+      .then(item => {
+        newObj[name] = item
+        this.setState(newObj)
+      })
+      .then(() => this.props.history.push(`/${name}`))
+  }
+
+  addItem = (name, item) => {
+    let newObj = {}
+    APIManager.post(name, item)
+      .then(() =>
+        APIManager.getAll(
+          `${name}?user_id=${+sessionStorage.getItem("activeUser")}`
+        )
+      )
+      .then(items => {
+        newObj[name] = items
+        this.setState(newObj)
+      })
+      .then(() => this.props.history.push("/"))
+      .then(() => this.props.history.push(`/${name}`))
+  }
+  addMessage = (name, item) => {
+    let newObj = {}
+    APIManager.post(name, item)
+      .then(() =>
+        APIManager.getAll(
+          `${name}`
+        )
+      )
+      .then(items => {
+        newObj[name] = items
+        this.setState(newObj)
+      })
+      .then(() => this.props.history.push(`/${name}`))
+  }
+
+  //loading user data to update state object
+  componentDidMount() {
+    const newState = {}
+    APIManager.getAll("users")
+      .then(allUsers => (newState.users = allUsers))
+      .then(() => APIManager.getAll("friends"))
+      .then(allFriends => (newState.friends = allFriends))
+      .then(() =>
+        APIManager.getAll("locations"))
+      .then(allLocations => (newState.locations = allLocations))
+      .then(() => this.setState(newState))
+  }
+
+  // check session storage for value, return true or false
+  isAuthenticated = () => {
+    return sessionStorage.getItem("activeUser") !== null
+  }
+
+  render() {
+
+    //route to all views
+    //if "isAuthenticated" returns true, then routes are accessible to users
+    //otherwise, route to welcome page
+
+    return (
+      <React.Fragment>
+        <Route
+          exact
+          path="/"
+          render={props => {
+            if (this.isAuthenticated()) return <div>welcome to prime time</div>
+            else return <Redirect to="/welcome" />
+          }}
+        />
+
+        <Route
+          exact
+          path="/welcome"
+          render={props => {
+            // Render a default page if user is not signed in
+            return <Welcome {...props} />
+          }}
+        />
+
+        <Route
+          exact
+          path="/login"
+          render={props => {
+            return <Login setUser={this.props.setUser} {...props} />
+          }}
+        />
+
+        <Route
+          exact
+          path="/register"
+          render={props => {
+            return <Register setUser={this.props.setUser} {...props} />
+          }}
+        />
+      </React.Fragment>
+    )
+  }
+}
+
+export default withRouter(ApplicationViews)
---
+++ src/components/nav/NavBar.js
@@ -0,0 +1,44 @@
+//Links to diffent routes
+import React, { Component } from "react"
+import { Link } from "react-router-dom"
+// import "bootstrap/dist/css/bootstrap.min.css"
+
+
+class NavBar extends Component {
+
+    logout = () => {
+        sessionStorage.clear()
+    }
+
+    render() {
+        return (
+            <nav className="navbar navbar-light light-blue flex-md-nowrap p-0 shadow">
+                <ul className="nav nav-pills nav-fill">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/articles">Articles</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/friends">Friends</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/messages">Messages</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/tasks">Tasks</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/events">Events</Link>
+                    </li>
+                    <Link className="nav-link"  to="/welcome">
+                        <button onClick={this.logout} className="nav-item">Logout</button>
+                    </Link>
+                </ul>
+            </nav>
+        )
+    }
+}
+
+export default NavBar
